81dbed66e32e2426283db7e48b5f0da12e059c12
pmk7 <72909830+pmk7@users.noreply.github.com>
2021-08-10 13:43:00 +0100

    third commit, rendered movie component

--- src/context.js
+++ src/context.js
@@ -7,7 +7,37 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState({ show: false, msg: '' });
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState('batman');
+
+  const fetchMovies = async (url) => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (data.Response === 'True') {
+        setMovies(data.Search);
+        setError({ show: false, msg: '' });
+      } else {
+        setError({ show: true, msg: data.Error });
+      }
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}&s=${query}`);
+  }, [query]);
+
+  return (
+    <AppContext.Provider value={{ isLoading, error, movies, query, setQuery }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 // make sure use
 export const useGlobalContext = () => {
